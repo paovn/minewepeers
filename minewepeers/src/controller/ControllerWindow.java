2f9f25d5f4f145d86a38bcea0090afbeeca10435
Usuario <Usuario@172.30.163.47>
2018-08-11 11:34:14 -0500

    Creation package and class controller

---
+++ minewepeers/src/controller/ControllerWindow.java
@@ -0,0 +1,19 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class ControllerWindow implements Initializable{
+	
+	private Button bBegginer;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
